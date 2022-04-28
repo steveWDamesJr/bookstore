6b3c88fb1c0d780e5920ea077773f661b350799d
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-04-28 08:08:30 -0500

    Initialize Project
    -Add React Redux
    -Structure application using feature folder with ducks pattern
    -Use Reusable components
    -Add React Router with two Routes and Links for navigation
    -Books default view; shows list of books with Remove button
    -Create component Book to display single book and reuse it in component BookList that displays list of books
    -Create form for adding book in separate component
    -Categories displays Under construction text
    -Add some Styles

---
+++ src/components/BookList.js
@@ -0,0 +1,11 @@
+import BookItem from './BookItem';
+
+const BookList = () => (
+  <div>
+    <BookItem title="THE HUNGER GAMES 1" author="Suzanne Collins" />
+    <BookItem title="THE HUNGER GAMES 2" author="Suzanne Collins" />
+    <BookItem title="THE HUNGER GAMES 3" author="Suzanne Collins" />
+  </div>
+);
+
+export default BookList;
